483bb57cf9073c468f611fd9975ff2e4cd12e6d4
Thomas Austin <thomasaustin@gmail.com>
2019-06-05 09:12:49 +0100

    create www record

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,15 @@ terraform {
   }
 }
 
+data "aws_route53_zone" "selected" {
+  name = "${var.domain}."
+}
+
 module "website" {
   source    = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=master"
   namespace = "solidsrc"
   stage     = "prod"
   name      = "web"
-  hostname  = "www.solidsrc.co.uk"
+  hostname  = "www.${var.domain}"
+  parent_zone_id = "${data.aws_route53_zone.selected.zone_id}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,1 @@
+variable "domain" { default = "solidsrc.co.uk" }
